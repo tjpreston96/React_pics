c55ae256872119410666bcc1a7a8357fafb4df43
tpreston96 <tpreston96@gmail.com>
2021-02-20 15:12:15 -0600

    Image search input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
 const App = () => {
-    return ( <div>Hi there!</div> );
-}
- 
+  return (
+    <div className="ui container" style={{ marginTop: "10px" }}>
+      <SearchBar />
+    </div>
+  );
+};
+
 export default App;
- 
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
