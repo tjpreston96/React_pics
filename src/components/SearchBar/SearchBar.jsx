88457b3d6867dbceb90e6b0d1b5e863d3e36ae9c
tpreston96 <tpreston96@gmail.com>
2021-02-20 15:32:12 -0600

    SearchBar onChange evtHandler

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 
 class SearchBar extends Component {
   state = {};
+
+  onInputChange(evt) {
+    console.log(evt.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
