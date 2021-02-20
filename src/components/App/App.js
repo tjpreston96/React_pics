532375051c130e74ebfc82fb7fa68754c9a27752
tpreston96 <tpreston96@gmail.com>
2021-02-20 17:38:43 -0600

    Custom client

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../../api/unsplash";
 import SearchBar from "../SearchBar/SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
+    const response = await unsplash.get("/search/photos", {
       params: { query: term },
-      headers: {
-        Authorization: "Client-ID utTmlDcVxmP8XnooUUPBk4VuFE_qzApfsVu8LvyFRX8",
-      },
     });
 
     this.setState({ images: response.data.results });
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
 
 // class App extends React.Component {
 //   state = {  }
-  
-//   render() { 
+
+//   render() {
 //     return ( <div>Hi there!</div> );
 //   }
 // }
- 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
