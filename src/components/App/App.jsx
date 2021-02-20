d74a09243faf0e1c051e89b485a8f806db19d0c4
tpreston96 <tpreston96@gmail.com>
2021-02-20 17:01:14 -0600

    API request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "../SearchBar/SearchBar";
 
 class App extends React.Component {
   state = {};
 
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID utTmlDcVxmP8XnooUUPBk4VuFE_qzApfsVu8LvyFRX8",
+      },
+    });
   }
 
   render() {
