2c6fe26f0787cde1ee65e08a016811dba66bc267
tpreston96 <tpreston96@gmail.com>
2021-02-20 16:06:38 -0600

    preventDefault

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,10 +3,14 @@ import React, { Component } from "react";
 class SearchBar extends Component {
   state = { term: "" };
 
+  onFormSubmit(evt) {
+    evt.preventDefault();
+    // console.log(this.state.term);
+  }
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
