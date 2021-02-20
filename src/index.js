621f878005b2a0b456ff834727107f068f821e9c
tpreston96 <tpreston96@gmail.com>
2021-02-20 14:44:09 -0600

    refactor w components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App/App'
 
-class App extends React.Component {
-  state = {  }
+// class App extends React.Component {
+//   state = {  }
   
-  render() { 
-    return ( <div>Hi there!</div> );
-  }
-}
+//   render() { 
+//     return ( <div>Hi there!</div> );
+//   }
+// }
  
 
 ReactDOM.render(
